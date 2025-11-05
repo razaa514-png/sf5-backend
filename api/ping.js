a87e80e964ee08b08f2b51d9a82527d5ccb8e046
razaa514-png <razaa514@gmail.com>
2025-11-05 14:56:15 -0500

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "pong" });
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return res.status(200).json({ message: "pong" });
 }
